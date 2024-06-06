d12182119e1c1dcd3de638cc7c9977706594b339
Baptiste LOISEAU <baptiste@eskafactory.io>
2024-06-06 21:36:42 +0200

    feat: contact email mailjet

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import { Express, Router } from "express";
 import path from "path";
 
@@ -18,15 +18,18 @@ const favoriteRoutes: Router = require("./routes/favorite");
 const bottleRoutes: Router = require("./routes/bottle");
 const commentRoutes: Router = require("./routes/comment");
 const adminRoutes: Router = require("./routes/admin");
+const emailRoutes: Router = require("./routes/email");
 
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(helmet());
 
 app.use("/api", defaultRoutes);
@@ -38,3 +41,4 @@ app.use("/api/favorite", favoriteRoutes);
 app.use("/api/bottle", bottleRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/email", emailRoutes);
